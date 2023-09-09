940b7b7b9e72a4bc47cfa986731fd8dccd51c53c
Lucía Amor <141635491+lu-amor@users.noreply.github.com>
2023-09-08 23:10:24 -0300

    falta corregir ProductionCost

---
+++ src/Library/Printer.cs
@@ -0,0 +1,29 @@
+//-------------------------------------------------------------------------------
+// <copyright file="Step.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------------
+using System;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class Printer
+    {
+        public Printer(Recipe recipe)
+        {
+            ArrayList recipeSteps = recipe.GetSteps();
+            Console.WriteLine($"Receta de {recipe.FinalProduct.Description}:");
+            foreach (Step step in recipe.steps)
+            {
+                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
+                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+                Console.WriteLine($"El costo total es de ${this.recipe.productionCost}");
+            }
+        }
+    }
+}
+
+/* Aplico el principio de responsabilidad única ya que no es responsabilidad de la receta saber imprimirse
+además así la clase de Recipe se puede reutilizar en caso de querer imprimir la receta en otro lado o escribirla en un archivo*/
---
+++ src/Library/ProductionCost.cs
@@ -0,0 +1,31 @@
+//-------------------------------------------------------------------------------
+// <copyright file="Step.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------------
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class ProductionCost
+    {
+        public double productionCost = 0;
+        public ProductionCost(Recipe recipe, double productionCost)
+        {
+            this.productionCost = productionCost;
+            double productCost = 0;
+            double equipmentCost = 0;
+            ArrayList recipeSteps = recipe.GetSteps();
+
+            foreach (Step step in recipeSteps)
+            {
+                productCost += GetProduct(step.Input.Description).UnitCost * step.Quantity;
+            }
+            foreach (Step step in recipeSteps)
+            {
+                equipmentCost += GetEquipment(step.Equipment.Description).HourlyCost * step.Time;
+            }
+            productionCost = productCost + equipmentCost;
+            return productionCost;
+        }
+    }
+}
